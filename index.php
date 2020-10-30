db5e602a249d004a893ae01e92b12f7a6e750c55
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-10-30 12:48:26 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+$servername = 'sql7.freemysqlhosting.net';
+$username = 'sql7373528';
+$password = 'SnSvdZtxTb';
+$dbname = 'sql7373528';
+$conn = new mysqli($servername, $username, $password, $dbname);
+$result = $conn->query('SELECT*FROM pracownicy where imie not like "%a" and zarobki<35 and
+zarobki>20');
+echo('<table border = 1>');
+echo('<th>id</th>');
+echo('<th>imie</th>');
+echo('<th>dzial</th>');
+echo('<th>zarobki</th>');
+while ($row = $result->fetch_assoc()){
+echo('<tr>');
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td>
+<td>'.$row['zarobki'].'</td>');
+echo('</tr>');
+}
+echo('</table>');
+
+?>
