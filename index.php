6d3d740eb32494e0aed589c4303d1eccf4275a82
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-10-30 13:08:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once("connect.php");
-$conn = new mysqli($servername, $username, $password, $dbname);
 $result = $conn->query('SELECT*FROM pracownicy where imie not like "%a" and zarobki<35 and
 zarobki>20');
 echo('<table border = 1>');
@@ -15,5 +14,19 @@ echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzi
 echo('</tr>');
 }
 echo('</table>');
+
+$result = $conn->query('SELECT*FROM pracownicy where imie like "%a" and zarobki<35 and
+zarobki>20');
+echo('<table border = 1>');
+echo('<th>id</th>');
+echo('<th>imie</th>');
+echo('<th>dzial</th>');
+echo('<th>zarobki</th>');
+while ($row = $result->fetch_assoc()){
+echo('<tr>');
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td>
+<td>'.$row['zarobki'].'</td>');
+echo('</tr>');
+}
 
 ?>
