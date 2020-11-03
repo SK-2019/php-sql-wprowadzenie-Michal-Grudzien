f8ec065d876b5481e774053b186da8fb176310ce
Tomjand <domagala.tomasz@gmail.com>
2020-11-03 13:17:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,20 +6,20 @@
 <body>
   <h1>Michal Grudzien 2Ti</h1>
 <?php
-// require_once("connect.php");
-// $conn = new mysqli("sql7.freemysqlhosting.net","sql7373528","SnSvdZtxTb","sql7373528");
-// $result = $conn->query('SELECT*FROM pracownicy where imie not like "%a" and zarobki<30');
-// echo('<table border = 1>');
-// echo('<th>id</th>');
-// echo('<th>imie</th>');
-// echo('<th>dzial</th>');
-// echo('<th>zarobki</th>');
-// while ($row = $result->fetch_assoc()){
-// echo('<tr>');
-// echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td>');
-// echo('</tr>');
-// }
-// echo('</table>');
+require_once("connect.php");
+$conn = new mysqli("sql7.freemysqlhosting.net","sql7373528","SnSvdZtxTb","sql7373528");
+$result = $conn->query('SELECT*FROM pracownicy where imie not like "%a" and zarobki<30');
+echo('<table border = 1>');
+echo('<th>id</th>');
+echo('<th>imie</th>');
+echo('<th>dzial</th>');
+echo('<th>zarobki</th>');
+while ($row = $result->fetch_assoc()){
+echo('<tr>');
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td>');
+echo('</tr>');
+}
+echo('</table>');
 
 ?>
 </body>
