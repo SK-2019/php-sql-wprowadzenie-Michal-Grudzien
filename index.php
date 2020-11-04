6a361ffb7c2c097d58e819cca063e88d0bf417d8
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-04 14:43:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,19 @@ echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzi
 echo('</tr>');
 }
 echo('</table>');
+  
+$result = $conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+echo('<table border = 1>');
+echo('<th>id</th>');
+echo('<th>imie</th>');
+echo('<th>dzial</th>');
+echo('<th>zarobki</th>');
+while ($row = $result->fetch_assoc()){
+echo('<tr>');
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td>');
+echo('</tr>');
+}
+echo('</table>');
 
 ?>
 </body>
