70225c290b47437aba439c3abd86882f99d81308
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 13:08:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
   <div class="nav">
      <a class="nav_link" href="pracownicy.php">Pracownicy wstęp</a>
      <a class="nav_link" href="funkcjeagregujace.php">Funkcje agregujace</a>
+    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
+   <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
   </div>
 <?php
   
