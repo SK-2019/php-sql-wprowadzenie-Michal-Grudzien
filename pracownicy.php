f4443a8e28dcb3e39f4d5b9ae8818b35a5e0b5f1
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 13:00:39 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -26,6 +26,8 @@
        
   
     $result = $conn->query('SELECT  * FROM pracownicy WHERE dzial=2');
+      echo("<hr />");
+        echo("<h3>Tabela pracowników z działu 2</h3>"); 
         echo("<table>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
@@ -40,6 +42,8 @@
     
     
     $result = $conn->query('SELECT  * FROM pracownicy WHERE dzial=2 or dzial=3');
+      echo("<hr />");
+        echo("<h3>Tabela pracowników z działu 2 i 3</h3>"); 
         echo("<table>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
@@ -51,6 +55,22 @@
         echo("</tr>");
     }
         echo("</table>");
+    
+    $result = $conn->query('SELECT imie, dzial, zarobki, FROM `pracownicy`, `organizacja` WHERE dzial = id_org and zarobki<30 and (dzial=1 or dzial=2)');
+     echo("<hr />");
+                echo("<h3>Tabela pracowników z zarobkiami mniejszymi niz 30</h3>");
+                echo("<table>");
+                echo("<th>imie</th>");
+                echo("<th>dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>data_urodzenia</th>");
+                     while($row=$result->fetch_assoc()){ 
+                       echo("<tr>");
+                            echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+        
+                        echo("</tr>");
+                    }
+                       echo("</table>");
     
     
     
