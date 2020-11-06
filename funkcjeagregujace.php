9f79c452d32f4456478c856eba9f4313bc21d8af
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 09:12:36 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -1,3 +1,13 @@
-<?php
-echo("Tu są: Funkcje_Agregujace.");
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Michal-Grudzien">GitHub</a>
+<div class="nav">
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+</div>
+</head>
+<body>
+  
 ?>
