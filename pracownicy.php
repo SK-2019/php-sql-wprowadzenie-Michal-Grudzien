a3d15b2395287222049e88f62693235ebbe471bd
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 09:09:13 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,4 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<div class="nav">
+    <a class="nav_link" href="pracownicy.php">Pracownicy wstęp</a>
+    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+</div>
+</head>
+<body>
 <?php
-echo("Tu są: Pracownicy");
-
-?>
+    echo("<hr />");
+    require_once('conn.php');;
+    $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+        echo("<h3>Tabela Pracowników</h3>");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+        echo("<hr />");
+        ?>
