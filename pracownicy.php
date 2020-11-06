959bc7a9d4c020a91a576025021e143c4030cfc2
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 13:11:24 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -5,6 +5,8 @@
 <div class="nav">
     <a class="nav_link" href="pracownicy.php">Pracownicy wstęp</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
+   <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
 </div>
 </head>
 <body>
