3959a52008db12fc3dec481a19e4891295083e4a
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 12:52:03 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -9,22 +9,36 @@
 </head>
 <body>
 <?php
-    echo("<hr />");
-    require_once('conn.php');;
-    $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+  require_once('conn.php');;
+   $result= $conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');
         echo("<h3>Tabela Pracowników</h3>");
         echo("<table border=1>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
+        while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
-        echo("<hr />");
+       
+  
+    $result = $conn->query('SELECT  * FROM pracownicy WHERE dzial=2');
+        echo("<table>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+                while($row = $result->fetch_assoc()) {
+        echo("<tr>");
+        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+        echo("</tr>");
+    }
+        echo("</table>");
+    
+    
+         
+    
         ?>
