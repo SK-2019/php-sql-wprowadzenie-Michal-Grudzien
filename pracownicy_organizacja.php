389acac4229925709e2b6c32b14c134066025e33
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-06 13:19:38 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -11,6 +11,22 @@
    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
    <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
    
+        require_once('connect.php');
+   $result= $conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+   echo("<hr />");
+        echo("<h3>Tabela Pracowników</h3>")
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+   while($row=$result->fetch_assoc()){
+         echo("<tr>");
+         echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>);
+            }
+        echo("</table>");
+    echo("<hr />");
+   
    
    ?>
 </body>
