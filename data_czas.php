caa518437600e58261fd156ca94c08199341b656
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-13 08:40:51 +0100

    Create data_czas.php

---
+++ data_czas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Michal-Grudzien"> GitHub </a>
+<div class="nav">
+ <a class="nav_link" href="pracownicy.php">Pracownicy wstęp</a>
+ <a class="nav_link" href="funkcjeagregujace.php">Funkcje agregujace</a>
+ <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
+ <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
+    
+</div>
+</head>
+<body>
+<?php
+
+
+
+
+
+
+
+
+ ?>
+</body>
+   </html>
