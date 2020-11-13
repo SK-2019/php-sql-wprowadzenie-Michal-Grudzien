b10f0efbee2c3e692c3b1c796b54ffbfc77287f1
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-13 09:17:47 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -8,6 +8,7 @@
  <a class="nav_link" href="funkcjeagregujace.php">Funkcje agregujace</a>
  <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
  <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
+ <a class="nav_link" href="data_czas.php">Data i Czas</a>
     
 </div>
 </head>
