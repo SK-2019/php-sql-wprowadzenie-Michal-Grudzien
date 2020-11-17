0ca1fab683f26444cc37c1cc94a1cdce9ab727b7
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-17 13:14:35 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -4,9 +4,13 @@ echo("jestes na strona.php");
 echo("</br>");
 
 echo($_POST["imie"]);
+echo("</br>");
 echo($_POST["nazwisko"]);
+echo("</br>");
 echo($_POST["pesel"]);
+echo("</br>");
 echo($_POST["urodziny"]);
+echo("</br>");
 echo($_POST["adres"]);
 
 
