15ace46ba902ab0cd13adfca8b0f98b65c4ff371
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-18 14:54:51 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -18,7 +18,7 @@ echo("<h3>Adres</h3>");
 echo($_POST["adres"]);
 
 $conn = new mysqli( "mysql-michalgrudzien04.alwaysdata.net" , "218517" , "ZAKo140" , "michalgrudzien04_baza");
-$sql = "INSERT INTO spaLcr81av(`id_pracownicy`, `imie`) VALUES (NULL,'".$_POST['imie']."')";
+$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', NULL, NULL, NULL)";
 	$conn->query($sql);
 if ($conn->query($sql) === TRUE) {
     echo "utworzono";
