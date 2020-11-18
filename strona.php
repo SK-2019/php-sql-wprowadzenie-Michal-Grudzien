d894151cad5d13b7faab30cc7d1044f00346520e
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-18 14:46:07 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -2,15 +2,19 @@
 
 echo("jestes na strona.php");
 echo("</br>");
-
+echo("<h3>Imie</h3>");
 echo($_POST["imie"]);
 echo("</br>");
+echo("<h3>Nazwisko/h3>");
 echo($_POST["nazwisko"]);
 echo("</br>");
+echo("<h3>Pesel</h3>");
 echo($_POST["pesel"]);
 echo("</br>");
+echo("<h3>Urodziny</h3>");
 echo($_POST["urodziny"]);
 echo("</br>");
+echo("<h3>Adres</h3>");
 echo($_POST["adres"]);
 
 $conn = new mysqli( "mysql-michalgrudzien04.alwaysdata.net" , "218517" , "ZAKo140" , "michalgrudzien04_baza");
