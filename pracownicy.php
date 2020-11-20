8e61e5cb3ea8f3bd0c067b4c8688dce6f6c4cda2
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-20 13:50:30 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -8,6 +8,7 @@
     <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
    <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
         <a class="nav_link" href="data_czas.php">Data i Czas</a>
+        <a class="nav_link" href="Danedobazy.html"><b>DaneDoBazy<b></a>
 </div>
 </head>
 <body>
