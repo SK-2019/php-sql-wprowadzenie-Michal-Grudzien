46a4c15b225b6a8d1473e1c8165663abd1e43d18
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-24 13:04:44 +0100

    Update function.php

--- function.php
+++ function.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head> 
+<body>
+<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Michal-Grudzien">Github</a>
+  <div class="nav">
+     <a class="nav_link" href="pracownicy.php">Pracownicy wstęp</a>
+     <a class="nav_link" href="funkcjeagregujace.php">Funkcje agregujace</a>
+    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
+   <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
+    <a class="nav_link" href="data_czas.php">Data i Czas</a>
+    <a class="nav_link" href="Danedobazy.html"><b>DaneDoBazy<b></a>
+     <a class="nav_link" href="function.php"><b>Funkcja<b></a>                                                    
+  </div>
+  
 <?php
 echo("abc");
 
@@ -12,3 +29,5 @@ funkcja(33);
 
 
 ?>
+</body>
+   </html>
