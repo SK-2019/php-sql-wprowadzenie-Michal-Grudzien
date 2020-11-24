83c397ddb02e3337534e6a6685a75576fb9a82a6
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-24 13:01:01 +0100

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,14 @@
 <?php
+echo("abc");
 
-echo(Funkcje);
+function funkcja1($liczba){
+  echo("<li>dziala funkcja");
+  echo("<li>proba ".$liczba);
+  for($i=1;$i<$liczba;$i++){
+    echo("<li>petla: ".$i);
+  }
+}
+funkcja(33);
 
 
 ?>
