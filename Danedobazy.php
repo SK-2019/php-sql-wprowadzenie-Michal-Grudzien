576fb5d7f6d5b2d7c6c2a31718cfcb57b5ed2cc0
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-11-27 12:48:43 +0100

    Update and rename Danedobazy.html to Danedobazy.php

--- Danedobazy.php
+++ Danedobazy.php
@@ -11,7 +11,7 @@
     <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
    <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
     <a class="nav_link" href="data_czas.php">Data i Czas</a>
-    <a class="nav_link" href="Danedobazy.html"><b>DaneDoBazy<b></a>
+    <a class="nav_link" href="Danedobazy.php"><b>DaneDoBazy<b></a>
   </div>
       <h3>Dodawanie Pracownika do tabelki</h3>
   <form action="insert.php" method="POST">
@@ -26,6 +26,31 @@
   <form action="delete.php" method="POST">
     id: <input type="text" name="id" placeholder="ID"></br>
    <input type="submit" value="Zapisz w zmiennej $_POST['id']">
+<?php
+
+echo("<hr />");
+$conn = new mysqli( "mysql-michalgrudzien04.alwaysdata.net" , "218517" , "ZAKo140" , "michalgrudzien04_baza");  
+$result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');       
+        echo("<table>");      
+        echo("<th>id</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>DataUrodzenia</th>");
+        echo("<th>NazwaDzialu</th>");
+        echo("<th>Usun</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("<td><form action='delete.php' method=POST>");
+                     echo("<input type='hidden' name='id' value='".$row['id_pracownicy']."'>");
+                     echo("<input type=submit value='Usun'>");
+                    echo("</form></td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+           
+?>
 </form>
 </body>
 </head>
