ce834643ac4bb84612384e84f225838a2ba6a904
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2020-12-04 08:14:38 +0100

    Create biblioteka.php

---
+++ biblioteka.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head> 
+<body>
+  <h1>Michal Grudzien 2Ti</h1>
+  <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Michal-Grudzien">Github</a>
+  <div class="nav">
+     <a class="nav_link" href="pracownicy.php">Pracownicy wstęp</a>
+     <a class="nav_link" href="funkcjeagregujace.php">Funkcje agregujace</a>
+    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy i Organizacja<b></a>
+   <a class="nav_link" href="https://michal-grudzien-indexphp.herokuapp.com">Strona Glowna</a>
+    <a class="nav_link" href="data_czas.php">Data i Czas</a>
+    <a class="nav_link" href="Danedobazy.php"><b>DaneDoBazy<b></a>
+     <a class="nav_link" href="function.php"><b>Funkcja<b></a>
+     <a class="nav_link" href="biblioteka.php"><b>Ksiazki i Autorzy<b></a>    
+  </div>
+<?php
+$conn = new mysqli( "mysql-michalgrudzien04.alwaysdata.net" , "218517" , "ZAKo140" , "michalgrudzien04_baza");  
+
+
+
+
+?>
+</body>
+   </html>
