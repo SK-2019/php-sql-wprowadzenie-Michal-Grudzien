c460b429c88503859e1c9d555e04a216088a45fd
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2021-02-02 13:44:08 +0100

    Update pracownicy.php

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -3,6 +3,7 @@
 <head>
 <title>Michał Grudzień 2Ti gr2</title>
 <link rel="stylesheet" href="assets/style.css">
+<link rel="icon" type="image/ico" sizes="16x16" href="../assets/favicon.ico">
 </head> 
  <body>
     <div class="container">
