625cafa437ed8822a6c17294c701b44ff76e5b9e
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2021-02-23 13:29:05 +0100

    Update connect.php

--- pracownicy/connect.php
+++ pracownicy/connect.php
@@ -1,12 +1,9 @@
 <?php
-$servername = 'mysql-michalgrudzien04.alwaysdata.net';
-$username = '218517';
-$password = 'ZAKo140';
-$dbname = 'michalgrudzien04_baza';
 
-//Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-//Check connection
-if ($conn->connect_error) {
-die('Connection failed: '. $conn->connection_error);
+    $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
+    if ($conn->connect_error) {
+        die("connection failed: ".mysqli_connect_error());
+    }
+
+
 ?>
