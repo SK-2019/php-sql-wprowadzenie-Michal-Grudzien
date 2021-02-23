9b84723da3740e8ed0a482699378b205e1bb60d7
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2021-02-23 13:42:55 +0100

    Update connect.php

--- pracownicy/connect.php
+++ pracownicy/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
+    $conn= new mysqli($_SERVER['mysqlservername'],$_SERVER['mysqlusername'],$_SERVER['mysqlpassword'],$_SERVER['mysqldbname']);
     if ($conn->connect_error) {
         die("connection failed: ".mysqli_connect_error());
     }
