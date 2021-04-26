84f8d851043cb7600ebd5d331ea73719ec8c001b
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2021-04-26 23:43:44 +0200

    Update nav.php

--- assets/nav.php
+++ assets/nav.php
@@ -11,4 +11,6 @@
  <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/pracownicy/data_czas.php">DataCzas</a></li>
  <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/flexbox/index.html">Flexbox</a></li>
  <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/relacje/tytulautor.php">Relacja Autor-Tytul</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/relacje/osobarola.php">Relacja Osoba-Rola</a></li>
+
   
