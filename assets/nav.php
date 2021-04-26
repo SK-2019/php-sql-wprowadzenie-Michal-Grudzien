d7d3a3e2d98bfe005d20445ded7517e2f9a7b40d
Michal-Grudzien <73532514+Michal-Grudzien@users.noreply.github.com>
2021-04-26 22:52:21 +0200

    Create nav.php

---
+++ assets/nav.php
@@ -0,0 +1,14 @@
+<div class="row">
+<div class="col-2 col-s-2 menu">
+<ul>
+ <li class="item_link"><a class="" href="https://github.com/SK-2019/php-sql-wprowadzenie-Michal-Grudzien">GitHub</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/pracownicy/Danedobazy.php">DaneDoBazy</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/biblioteka/ksiazki.php">Biblioteka</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/index.php">StronaGlowna</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/pracownicy/funkcjeagregujace.php">FunkcjeAgregujace</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/pracownicy/pracownicy_organizacja.php">Pracownicy_Organizacja</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/pracownicy/pracownicy.php">Pracownicy</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/pracownicy/data_czas.php">DataCzas</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/flexbox/index.html">Flexbox</a></li>
+ <li class="item_link"><a class="" href="https://michal-grudzien-indexphp.herokuapp.com/relacje/tytul-autor.php">Flexbox</a></li>
+  
